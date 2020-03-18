a0ab94eff06b8afef155c4e624a0d2a90b8dbebb
stepanyan.1996.vs@gmail.com <Vach1996>
2020-03-18 17:45:48 +0400

    Add .gitignore file ignoring all IDE setting files

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         Dog shun = new Dog();
         shun.voice();
         System.out.println(shun.legs());
-        System.out.println(shun.tail());
+        boolean s1  = shun.tail();
+        if (s1 == true)
+            System.out.println("Shuny uni poch");
+         else
+            System.out.println("Shuny chuni poch");
+
         Cat katu = new Cat();
+
+
 
         katu.voice();
 
         System.out.println(katu.legs());
-        System.out.println(katu.tail());
+        boolean s2  = katu.tail();
+        if (s2 == true)
+            System.out.println("Katun uni poch");
+        else
+            System.out.println("Katun  chuni poch");
     }
 }
